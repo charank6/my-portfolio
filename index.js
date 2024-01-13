0551152fd0341b8187fdedd0b9c15e63e74e9ba9
charank6 <129172700+charank6@users.noreply.github.com>
2024-01-13 22:07:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,110 @@
+const buttons = document.querySelectorAll("button");
+
+const resultEl = document.getElementById("result");
+
+const playerScoreEl = document.getElementById("user-score");
+
+const computerScoreEl = document.getElementById("computer-score");
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const result = playRound(button.id, computerPlay());
+    resultEl.textContent = result;
+    
+  });
+});
+
+function computerPlay() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomChoice = Math.floor(Math.random() * choices.length);
+  return choices[randomChoice];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerScore++;
+    playerScoreEl.textContent = playerScore;
+    return "You win! " + playerSelection + " beats " + computerSelection;
+  } else {
+    computerScore++;
+    computerScoreEl.textContent = computerScore;
+    return "You lose! " + computerSelection + " beats " + playerSelection;
+  }
+}
+
+
+
+const formEl = document.querySelector(".form");
+
+const inputEl = document.querySelector(".input");
+
+const ulEl = document.querySelector(".list");
+
+let list = JSON.parse(localStorage.getItem("list"));
+if (list) {
+  list.forEach((task) => {
+    toDoList(task);
+  });
+}
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  toDoList();
+});
+
+function toDoList(task) {
+  let newTask = inputEl.value;
+  if (task) {
+    newTask = task.name;
+  }
+
+  const liEl = document.createElement("li");
+  if (task && task.checked) {
+    liEl.classList.add("checked");
+  }
+  liEl.innerText = newTask;
+  ulEl.appendChild(liEl);
+  inputEl.value = "";
+  const checkBtnEl = document.createElement("div");
+  checkBtnEl.innerHTML = `
+  <i class="fas fa-check-square">
+  `;
+  liEl.appendChild(checkBtnEl);
+  const trashBtnEl = document.createElement("div");
+  trashBtnEl.innerHTML = `
+  <i class="fas fa-trash"></i>
+  `;
+  liEl.appendChild(trashBtnEl);
+
+  checkBtnEl.addEventListener("click", () => {
+    liEl.classList.toggle("checked");
+    updateLocalStorage();
+  });
+
+  trashBtnEl.addEventListener("click", () => {
+    liEl.remove();
+    updateLocalStorage();
+  });
+  updateLocalStorage();
+}
+
+function updateLocalStorage() {
+  const liEls = document.querySelectorAll("li");
+  list = [];
+  liEls.forEach((liEl) => {
+    list.push({
+      name: liEl.innerText,
+      checked: liEl.classList.contains("checked"),
+    });
+  });
+  localStorage.setItem("list", JSON.stringify(list));
+}
